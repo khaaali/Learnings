7fbdf2f36f2044a3c11d5f0de89c73225079243f
samforiyana <sairamaaaa@gmail.com>
2022-07-16 20:20:55 +0200

    sliding window update

--- Leetcode/Arrays/DuplicateZeros.js
+++ Leetcode/Arrays/DuplicateZeros.js
@@ -8,7 +8,8 @@ Explanation: After calling your function, the input array is modified to: [1,0,0
 
 */
 
-var duplicateZeros = function (arr) {
+//  utilising and additional array
+var duplicateZerosWithArray = function (arr) {
 	let arr2 = [];
 	// if current element is 0, add 0 to next index and shift right
 	let count = 0;
@@ -22,16 +23,18 @@ var duplicateZeros = function (arr) {
 		}
 	}
 
-	arr = resizeArray(arr2, count);
-
-	return arr;
-};
-
-function resizeArray(arr2, cnt) {
-	for (let i = cnt; i > 0; i--) {
+	for (let i = count; i > 0; i--) {
 		arr2.pop();
 	}
-	return arr2;
-}
 
-console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
+	return arr2;
+};
+
+var duplicateZerosInplaceArray = function (arr) {
+
+
+	
+};
+
+console.log(duplicateZerosWithArray([1, 0, 2, 3, 0, 4, 5, 0]));
+console.log(duplicateZerosInplaceArray([1, 0, 2, 3, 0, 4, 5, 0]));
--- Leetcode/Arrays/SlidingWindow.js
+++ Leetcode/Arrays/SlidingWindow.js
@@ -8,10 +8,12 @@ We get maximum sum by adding subarray {4, 2, 10, 23} of size 4.
 */
 
 let arr = [1, 4, 2, 10, 2, 3, 1, 0, 20];
-SlidingWindowBruteForce(arr, 3);
+BruteForce(arr, 6);
+SlidingWindow(arr, 6);
 
 //  Complexity O(n2)
-function SlidingWindowBruteForce(array, windowSize) {
+function BruteForce(array, windowSize) {
+	if (windowSize > array.length) return;
 	let maxSum = 0;
 	// loop through outer array for elements length relative to window size
 	for (let out = 0; out <= array.length - windowSize; out++) {
@@ -25,16 +27,23 @@ function SlidingWindowBruteForce(array, windowSize) {
 	console.log(maxSum);
 }
 
-function SlidingWindowBruteForce(array, windowSize) {
+// O(n)
+function SlidingWindow(array, windowSize) {
+	if (windowSize > array.length) return;
 	let maxSum = 0;
-	// loop through outer array for elements length relative to window size
-	for (let out = 0; out <= array.length - windowSize; out++) {
-		let currentSum = 0;
-		// loop through the window size of sub array ->{1,4,2,10}
-		for (let inn = out; inn < out + windowSize; inn++) {
-			currentSum += array[inn];
+	let currentSum = 0;
+	// loop through array for elements length
+	for (let out = 0; out < array.length; out++) {
+		// when current out is > window size : our window size is reached and should be maintained
+		//  added current index element and delete first index element of current window
+		currentSum += array[out];
+		if (out > windowSize - 1) {
+			currentSum = currentSum - array[out - windowSize];
+			maxSum = Math.max(currentSum, maxSum);
 		}
-		maxSum = Math.max(currentSum, maxSum);
 	}
 	console.log(maxSum);
 }
+
+//  to learn
+// inplace array index element shift right
